1f8290a7ea25675227079e3e3bee16b50e7ef8d0
Rishal <Rishal.mhd009@gmail.com>
2023-08-17 15:58:55 +0530

    3 page layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import ChatPage from "./components/ChatPage";
+import LandingPage from "./components/LandingPage";
+import Confirmation from "./components/Confirmation";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "chat",
+        element: <ChatPage />,
+      },
+      {
+        path: "confirmation",
+        element: <Confirmation />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div>
-     <ChatPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/Confirmation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Confirmation() {
+  return (
+    <div>
+      <h1>balls</h1>
+    </div>
+  );
+}
+
+export default Confirmation;
