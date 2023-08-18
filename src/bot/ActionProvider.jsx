a72ded32b91dabafab31ba1dac89902670b8835f
Rishal <Rishal.mhd009@gmail.com>
2023-08-18 17:11:10 +0530

    got it worked

--- src/bot/ActionProvider.jsx
+++ src/bot/ActionProvider.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createClientMessage } from 'react-chatbot-kit';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const [gotItClicked, setGotItClicked] = useState(false)
   const handleGotIt = () => {
-    const userMessage = 'Got it!'; // The user's response
-
+    const userMessage = createClientMessage("Got it.")
     // Add the user's response to the messages
     setState((prev) => ({
       ...prev,
-      messages: [...prev.messages, userMessage],
-    }));
-
+      messages: [...prev.messages,userMessage],
+    })); 
+  
+    //remove the got it button 
+    
     // Now add the bot's response asking for the name
     const namePrompt = createChatBotMessage('Enter your Name');
-
+    
     setState((prev) => ({
       ...prev,
       messages: [...prev.messages, namePrompt],
     }));
+    
+    setGotItClicked(true)
   };
 
   return (
@@ -25,6 +30,7 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
         return React.cloneElement(child, {
           actions: {
             handleGotIt,
+            gotItClicked : gotItClicked,
           },
         });
       })}
--- src/bot/config.js
+++ src/bot/config.js
@@ -4,18 +4,25 @@ const config = {
   initialMessages: [
     createChatBotMessage(`Hello, Welcome to student info system!`, {
       withAvatar: true,
-      delay: 3000,
+      delay: 2000,
       widget: 'gotItButton',
     }),
   ],
   widgets: [
     {
       widgetName: 'gotItButton',
-      widgetFunc: (props) => (
-        <button className="got-it-button" onClick={props.actionProvider.handleGotIt}>
-          Got it!
-        </button>
-      ),
+      widgetFunc: (props) => {
+        console.log(props.actionProvider.gotItClicked);
+        if (!props.actionProvider.gotItClicked) {
+          return (
+            <button className="got-it-button" onClick={props.actionProvider.handleGotIt}>
+              Got it!
+            </button>
+          );
+        } else {
+          return null; // Don't render the button if it's already clicked
+        }
+      },
     },
   ],
 };
