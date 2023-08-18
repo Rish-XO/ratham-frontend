ee8e0eb37c8951c8c7b059f952485fa2375d6742
Rishal <Rishal.mhd009@gmail.com>
2023-08-18 18:55:28 +0530

    done

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Container, Grid, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
 function Confirmation() {
+  const name = useSelector((state) => state.chatHandler.name);
+  const age = useSelector((state) => state.chatHandler.age);
   return (
-    <div>
-      <h1>balls</h1>
-    </div>
+    <Container>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h4" fontWeight="bold" textAlign="center">
+          Your name {name} aged {age} has been added to student system. You may now exit.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
